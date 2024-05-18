84043885e510b98a67eef8e6751a0b6f4d06ce8c
c4dr-me <142721314+c4dr-me@users.noreply.github.com>
2024-05-18 15:30:20 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+import Navbar from "../components/navbar"
+import Hero from "../components/hero"
+import Card from "../components/card"
+import data from "../data"
+
+export default function App() {
+    const cards = data.map(item => {
+        return (
+            <Card
+                key={item.id}
+                {...item}
+                
+            />
+        )
+    })        
+    
+    return (
+        <div>
+            <Navbar />
+            <section className="align">
+            <Hero />
+            <section className="cards-list">
+                {cards}
+            </section>
+            </section>
+            
+        </div>
+    )
+}
