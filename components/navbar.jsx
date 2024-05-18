1a9cbb4e0ed1ec986838e7ad03012061caca32ea
c4dr-me <142721314+c4dr-me@users.noreply.github.com>
2024-05-18 16:06:45 +0530

    images

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -3,7 +3,7 @@ import react from 'react'
 export default function Navbar() {
     return (
         <nav>
-            <img src="../images/airbnb-logo.png" className="nav--logo"/>
+            <img src="/images/airbnb-logo.png" className="nav--logo"/>
         </nav>
     )
 }
